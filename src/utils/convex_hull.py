37d2247bde5acb757b0e45ef1791ad879aa6e776
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-04-24 23:56:56 -0400

    add StackOverflow reference

--- src/utils/convex_hull.py
+++ src/utils/convex_hull.py
@@ -4,13 +4,15 @@ import numpy as np
 
 def in_hull(p: np.ndarray, hull: np.ndarray):
   """
-    Test if points in `p` are in `hull`
+  Test if points in `p` are in `hull`
 
-    `p` should be a `NxK` coordinates of `N` points in `K` dimensions
-    `hull` is either a scipy.spatial.Delaunay object or the `MxK` array of the 
-    coordinates of `M` points in `K`dimensions for which Delaunay triangulation
-    will be computed
-    """
+  `p` should be a `NxK` coordinates of `N` points in `K` dimensions
+  `hull` is either a scipy.spatial.Delaunay object or the `MxK` array of the 
+  coordinates of `M` points in `K`dimensions for which Delaunay triangulation
+  will be computed
+
+  Referenced from: https://stackoverflow.com/a/16898636
+  """
   if not isinstance(hull, Delaunay):
     hull = Delaunay(hull)
 
